55b0cfe4ad45b2e4760cff13a92ee0d96743cfc0
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-04-22 10:53:46 -1000

    Added methods to Processor and Memory to get basic functionality for Console

--- src/Memory.java
+++ src/Memory.java
@@ -1,5 +1,62 @@
 public class Memory {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+    private int[] cell; 
+    private int cap = 256; 
+
+    /**
+     * Constructor that sets an initial capacity of 256
+     */
+    public Memory() {
+        this.cell = new int[cap]; 
+    }
+
+    /**
+     * Constructor that specifies the amount of memory to allocate
+     * 
+     * @param cap
+     *  The amount of memory to allocate. 
+     */
+    public Memory(int cap) {
+        this.cap = cap; 
+        this.cell = new int[cap]; 
+    }
+
+    /**
+     * Get the value stored in the specific memory address. 
+     * 
+     * @param address
+     *  The address of where the value is stored. 
+     * @return
+     *  Integer value stored in the specified address. 
+     */
+    public int read(int address) {
+        return cell[address]; 
+    }
+
+    /**
+     * Write the value to the memory address. 
+     * @param address
+     *  Memory address location that you want to store the data in. 
+     * @param data
+     *  Value that you want to put in the address. 
+     */
+    public void write(int address, int data) {
+        cell[address] = data; 
+    }
+
+    /**
+     * Dump the memory contents into the console. When it dumps the memory contents, it will translate them into the 
+     * into the hexadecimal representation of the values.
+     * 
+     */
+    public void dump() {
+        System.out.println("Memory: \n");
+        for (int i = 0; i < cell.length; i++) {
+            System.out.printf("Cell[%d] = %s %n", i, getHex(cell[i]));
+        }
+    }
+
+    public static String getHex(int decimal) {
+        return Integer.toHexString(decimal); 
     }
 }
--- src/Processor.java
+++ src/Processor.java
@@ -1,5 +1,58 @@
 public class Processor {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private int[] reg; 
+    private int PC; 
+    private int IR; 
+    private Memory memory; 
+
+    public Processor() {
+        this.reg = new int[8]; 
+        this.PC = 0; 
     }
+
+    /**
+     * Allocates a specific amount of memory for the Processor. 
+     * 
+     * @param memory
+     *  The amount of memory allocated for the Processor. 
+     */
+    public void setMemory(Memory memory) {
+        this.memory = memory; 
+    }
+
+
+    /**
+     * Set the PC to point to the specified address. 
+     * @param address
+     *  The address that the program counter is pointing to. 
+     */
+    public void setPC(int address) {
+        this.PC = address; 
+    }
+
+    /**
+     * Read the instruction stored in the memory. 
+     * 
+     * @return
+     *  Boolean value representing the Instruction stored in the cell. 
+     */
+    public boolean step() {
+        IR = memory.read(PC++); 
+        return IR == 0; 
+    }
+
+    /**
+     * Dump the Processor information - Registers, PC, and IR into the Console. Values shown in the 
+     * Processor information is the hexadecimal representation. 
+     * 
+     */
+    public void dump() {
+        System.out.println("Registers:");
+        for (int i = 0; i < reg.length; i++) { 
+            System.out.printf("Register %d = %s %n", i, Memory.getHex(reg[i]));
+        }
+        System.out.println("PC = " + PC);
+        System.out.println("IR = " + Integer.toHexString(IR));
+    }
+
+    
 }
